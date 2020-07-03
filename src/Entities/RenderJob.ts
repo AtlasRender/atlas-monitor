ffd87a9619deb84d07e64096d4fce4f45e293275
DanilAndreev <DanilAndreev.DanSSG08@users.noreply.github.com>
2020-07-03 16:26:48 +0300

    some changes

--- src/Entities/RenderJob.ts
+++ src/Entities/RenderJob.ts
@@ -1,13 +1,28 @@
 export default class RenderJob {
+    public static QUEUED: number = 100;
+    public static PROCESSING: number = 200;
+    public static SUSPENDED: number = 201;
+    public static FAILED: number = 300;
+    public static COMPLETED: number = 400;
+
+
     public id: number;
     public department: string;
     public submitter: string;
     public submitDate: Date;
+    public status: number;
+
+    public files: string[];
+    public projectRoot: string;
+
 
     public constructor(data: any) {
         this.id = +data?.id;
         this.department = data?.department;
         this.submitter = data?.submitter;
         this.submitDate = data?.submitDate;
+        this.status = data?.status || RenderJob.QUEUED;
+        this.files = data?.files;
+        this.projectRoot = data?.projectRoot;
     }
 }
