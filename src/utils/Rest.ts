1cd8856c0f1325acbd77338dcd14e51c30a94b73
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-09-29 21:46:13 +0300

    [config] Added copyrighting profiles and run configs.

--- src/utils/Rest.ts
+++ src/utils/Rest.ts
@@ -1,5 +1,12 @@
-/* Author: Andrieiev Danil | danssg08@gmail.com | https://github.com/DanilAndreev
-   Copyright (C) 2020 */
+/*
+ * Copyright (c) 2020. This code created and belongs to Pathfinder render manager project.
+ * Owner and project architect: Danil Andreev | danssg08@gmail.com |  https://github.com/DanilAndreev
+ * File creator: Danil Andreev
+ * Project: pathfinder-monitor
+ * File last modified: 29.09.2020, 21:44
+ * All rights reserved.
+ */
+
 import superagent from "superagent";
 
 
