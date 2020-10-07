f90d182d160703ade180c75a7a2f5810c73d4092
ZaykaNya <48089558+ZaykaNya@users.noreply.github.com>
2020-10-07 17:07:13 +0300

    [DateField] Created DateField component

---
+++ src/components/DateField/DateField.tsx
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2020. This code created and belongs to Pathfinder render manager project.
+ * Owner and project architect: Danil Andreev | danssg08@gmail.com |  https://github.com/DanilAndreev
+ * File creator: Andrii Demchyshyn
+ * Project: pathfinder-monitor
+ * File last modified: 07.10.2020, 16:43
+ * All rights reserved.
+ */
+
+import 'date-fns';
+import React from 'react';
+import DateFnsUtils from '@date-io/date-fns';
+import {KeyboardDatePicker, MuiPickersUtilsProvider} from '@material-ui/pickers';
+import Stylable from "../../interfaces/Stylable";
+import {withStyles} from "@material-ui/core";
+import styles from "./styles";
+
+/**
+ * DateFieldProps - interface for DateField component
+ * @interface
+ * @author Andrii Demchyshyn
+ */
+interface DateFieldProps extends Stylable {
+}
+
+/**
+ * DateField - creates component where you can choose date
+ * @function
+ * @author Andrii Demchyshyn
+ */
+const DateField = React.forwardRef((props: DateFieldProps, ref: React.Ref<any>) => {
+
+    const [selectedDate, setSelectedDate] = React.useState<Date | null>(
+        new Date('2014-08-18T21:11:54'),
+    );
+
+    const handleDateChange = (date: Date | null) => {
+        setSelectedDate(date);
+    };
+
+    return (
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+                disableToolbar
+                variant="inline"
+                format="MM/dd/yyyy"
+                margin="normal"
+                id="date-picker-inline"
+                label="Choose date"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                }}
+            />
+        </MuiPickersUtilsProvider>
+    );
+});
+
+export default withStyles(styles)(DateField);
