5d014c16e00aa6d42f9a4810d35056e2dbc88601
ZaykaNya <48089558+ZaykaNya@users.noreply.github.com>
2020-10-15 20:39:22 +0300

    [StringValidator] Created StringValidator.

--- src/core/validators/StringValidator.ts
+++ src/core/validators/StringValidator.ts
@@ -3,21 +3,25 @@
  * Owner and project architect: Danil Andreev | danssg08@gmail.com |  https://github.com/DanilAndreev
  * File creator: Andrii Demchyshyn
  * Project: pathfinder-monitor
- * File last modified: 15.10.2020, 20:07
+ * File last modified: 15.10.2020, 20:34
  * All rights reserved.
  */
 
 import ValidatorResult from "../../interfaces/ValidatorResult";
 
 /**
- * NumberValidator - checks if value is number or not.
+ * StringValidator - checks if value is string or not.
  * @param value
  * @function
  * @author Andrii Demchyshyn
  */
-export default function NumberValidator(value: any): ValidatorResult<number>{
-    if(isNaN(+value)) {
+export default function StringValidator(value: any): ValidatorResult<string>{
+    let stringValue = value;
+    if(typeof stringValue === "number") {
+        stringValue = "" + stringValue;
+    }
+    if(typeof stringValue !== "string") {
         return {value: undefined, error: true};
     }
-    return {value: +value, error: false};
+    return {value: stringValue, error: false};
 }
