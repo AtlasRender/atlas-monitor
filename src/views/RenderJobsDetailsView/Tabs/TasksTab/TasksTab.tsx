ef577c06b4078622af023a1083e74c46492ff281
ZaykaNya <48089558+ZaykaNya@users.noreply.github.com>
2020-12-13 22:06:10 +0200

    [TasksLogs] Fixed update tasks event.

--- src/views/RenderJobsDetailsView/Tabs/TasksTab/TasksTab.tsx
+++ src/views/RenderJobsDetailsView/Tabs/TasksTab/TasksTab.tsx
@@ -242,7 +242,7 @@ const TasksTab = React.forwardRef((props: TasksTabProps, ref: Ref<any>) => {
                                         <TableCell align="left">Slave</TableCell>
                                         <TableCell align="left">{format(task.createdAt, "dd.MM.yyyy hh:mm")}</TableCell>
                                         <TableCell align="left">
-                                            {isWidthUp("md", props.width) ? (<Progress progress={task.progress}/>) : ("10%")}
+                                            {isWidthUp("md", props.width) ? (<Progress progress={task.status === "done" ? 100 : task.progress}/>) : ("10%")}
                                         </TableCell>
                                         <TableCell align="left">
                                             <IconButton className={classes.iconVisible} onClick={() => handleOpenDialog(task.id)}>
